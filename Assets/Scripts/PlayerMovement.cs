12c269682482f5f144e2b76cf3f18e9d3ae883f6
Daria Gronska <gronskaadariaa@gmail.com>
2026-02-22 23:45:05 +0100

    add sides movements

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float playerSpeed = 2f;
     public float horizontalSpeed = 3f;
+    public float rightLimit = 7.5f;
+    public float leftLimit = -7.5f;
+
 
 
     void Update()
@@ -13,11 +14,18 @@ public class PlayerMovement : MonoBehaviour
         transform.Translate(Vector3.forward * playerSpeed*Time.deltaTime, Space.World);
         if (Input.GetKey(KeyCode.A)||(Input.GetKey(KeyCode.LeftArrow)))
         {
-            transform.Translate(Vector3.left*horizontalSpeed*Time.deltaTime, Space.World);
+            if (this.gameObject.transform.position.x>leftLimit)
+            { 
+                transform.Translate(Vector3.left*horizontalSpeed*Time.deltaTime, Space.World);
+            }
+            
         }
         if (Input.GetKey(KeyCode.D)||(Input.GetKey(KeyCode.RightArrow)))
         {
-            transform.Translate(Vector3.right*horizontalSpeed*Time.deltaTime, Space.World);
+            if (this.gameObject.transform.position.x<rightLimit)
+            {
+                transform.Translate(Vector3.right*horizontalSpeed*Time.deltaTime, Space.World);
+            }
         }
     }
 }
